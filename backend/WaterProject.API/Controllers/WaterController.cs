ce2382572ac798bfd26ba3cb1b7b8d4453fad974
Kaitlyn <ksmetler@gmail.com>
2025-03-20 19:29:08 -0600

    more stuff

--- backend/WaterProject.API/Controllers/WaterController.cs
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -13,11 +13,33 @@ namespace WaterProject.API.Controllers
         public WaterController(WaterDbContext temp) => _waterContext = temp;
 
         [HttpGet("AllProjects")]
-        public IEnumerable<Project> GetProjects()
+        public IActionResult GetProjects(int pageSize = 10, int pageNum = 1)
         {
-            var something = _waterContext.Projects.ToList();
+            string? favProjectType = Request.Cookies["FavoriteProjectType"];
+            Console.WriteLine("-------COOKIE-------\n" +  favProjectType);
             
-            return something;
+            HttpContext.Response.Cookies.Append("FavoriteProjectType", "Borehole Well and Hand Pump", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.Now.AddMinutes(1),
+            });
+
+            var something = _waterContext.Projects
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalNumProjects = _waterContext.Projects.Count();
+
+            var someObject = new
+            {
+                Projects = something,
+                TotalNumProjects = totalNumProjects
+            };
+            
+            return Ok(someObject);
         }
 
         [HttpGet("FunctionalProjects")]
