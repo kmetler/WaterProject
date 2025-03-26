d5f10147741f049d260c5c28c78e9be5d1ed0910
Kaitlyn <ksmetler@gmail.com>
2025-03-26 12:33:44 -0600

    Add donate and cart functionality

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Project } from './types/Project';
+import { Project } from '../types/Project';
+import { useNavigate } from 'react-router-dom';
 
 function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -7,6 +8,7 @@ function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
   const [pageNum, setPageNum] = useState<number>(1);
   const [totalItems, setTotalItems] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -16,9 +18,6 @@ function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
 
       const response = await fetch(
         `https://localhost:5000/Water/AllProjects?pageSize=${pageSize}&pageNum=${pageNum}${selectedCategories.length ? `&${categoryParams}` : ''}`
-        // {
-        //   credentials: 'include',
-        // }
       );
       const data = await response.json();
       setProjects(data.projects);
@@ -57,6 +56,15 @@ function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
                 {p.projectFunctionalityStatus}
               </li>
             </ul>
+
+            <button
+              className="btn btn-success"
+              onClick={() =>
+                navigate(`/donate/${p.projectName}/${p.projectId}`)
+              }
+            >
+              Donate
+            </button>
           </div>
         </div>
       ))}
