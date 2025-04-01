90fab050121538cf5973ed28d183f7604e8f9247
Kaitlyn <ksmetler@gmail.com>
2025-04-01 10:32:31 -0600

    Getting ready to deploy

--- frontend/src/api/ProjectsAPI.ts
+++ frontend/src/api/ProjectsAPI.ts
@@ -5,7 +5,8 @@ interface FetchProjectsResponse {
   totalNumProjects: number;
 }
 
-const API_URL = 'https://localhost:5000/Water';
+// const API_URL = 'https://localhost:5000/Water';
+const API_URL = 'https://waterproject-metler-backend.azurewebsites.net/Water';
 
 export const fetchProjects = async (
   pageSize: number,
